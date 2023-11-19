abb8fcfa0efcd62c927d05f2f9df7d958d175fc2
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-11-19 16:38:03 +0100

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,50 @@
+
+using System;
+
+public class Solution
+{
+    private static readonly int[] DISTANCE_RANGE = { 1, (int)Math.Pow(10, 5) };
+    private static readonly int[] SPEED_RANGE = { 1, (int)Math.Pow(10, 5) };
+    private static readonly int MAX_TIME_TO_REACH_THE_CITY = (int)Math.Ceiling((double)DISTANCE_RANGE[1] / SPEED_RANGE[0]);
+    private readonly int[] numberOfMonstersToReachTheCityAtTimePoint = new int[MAX_TIME_TO_REACH_THE_CITY + 1];
+
+
+    public int EliminateMaximum(int[] distance, int[] speed)
+    {
+        int totalNumberOfMonsters = distance.Length;
+        initializeArray_numberOfMonstersToReachTheCityAtTimePoint(distance, speed);
+        return findMaxNumberOfEliminatedMonsters(totalNumberOfMonsters);
+    }
+
+    private void initializeArray_numberOfMonstersToReachTheCityAtTimePoint(int[] distance, int[] speed)
+    {
+        for (int i = 0; i < distance.Length; ++i)
+        {
+            ++numberOfMonstersToReachTheCityAtTimePoint[(int)Math.Ceiling((double)distance[i] / speed[i])];
+        }
+    }
+
+    private int findMaxNumberOfEliminatedMonsters(int totalNumberOfMonsters)
+    {
+        int eliminatedMonsters = 1;
+
+        for (int time = 0; time < numberOfMonstersToReachTheCityAtTimePoint.Length; ++time)
+        {
+
+            numberOfMonstersToReachTheCityAtTimePoint[time] += time > 0 ? numberOfMonstersToReachTheCityAtTimePoint[time - 1] : 0;
+
+            //can eliminate monsters only at a distance
+            if (eliminatedMonsters <= numberOfMonstersToReachTheCityAtTimePoint[time])
+            {
+                --eliminatedMonsters;
+                break;
+            }
+            if (eliminatedMonsters == totalNumberOfMonsters)
+            {
+                break;
+            }
+            ++eliminatedMonsters;
+        }
+        return eliminatedMonsters;
+    }
+}
